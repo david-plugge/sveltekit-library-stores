0527fbe0fd67c98acc56198d4dbbf5f2973c07a5
David Plugge <git@davidplugge.de>
2022-10-18 14:44:42 +0200

    fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,11 @@
 	import { user } from 'custom-package';
 
 	$: console.log('user from page store', $page.data.user);
-	$: console.log('user from ext. pacakge', $user);
+	$: console.log('user from custom-package', $user);
 </script>
 
 <h2>User from page store</h2>
 <pre>{JSON.stringify($page.data.user, null, 2)}</pre>
 
-<h2>User from external package</h2>
+<h2>User from custom-package</h2>
 <pre>{JSON.stringify($user, null, 2)}</pre>
